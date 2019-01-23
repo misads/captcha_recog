afffc204dfdf1b12704cdc3b07f87009fa9988d9
xhy <523131316@qq.com>
2019-01-23 19:29:19 +0800

    change default training steps

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ CAPTCHA_LENGTH = 4      # only work on capcha with fixed-length
 CHAR_SET_LEN = 36       # 10 digits and 26 lower-case letters
 DATASET_SIZE = 3000     # how many images in your train set
 BATCH_SIZE = 64         # how many samples to train within a batch
-TRAINING_STEPS = 1000  # how many steps to train before saving the model and exiting
+TRAINING_STEPS = 5000   # how many steps to train before saving the model and exiting
